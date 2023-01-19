26f2d9c0d877e6bf411205ed9b1703a9cd52ff6f
Luan Machado <94332377+luanmachadoyt@users.noreply.github.com>
2023-01-19 09:05:42 -0300

    Update script.js

--- assents/JS/script.js
+++ assents/JS/script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () =>
     .type('Streamer - Twitch!', {delay: 900}) .delete(18)
     .type('Youtuber!', {delay: 900}) .delete(9)
     .type('TikToker!', {delay: 900}) .delete(9)
-    .type('Futuro jogador do complexo!', {delay: 900}) .delete(27)
     .go()
 });
 
